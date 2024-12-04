4f5870189b8c6dc173ae0c785161c8ac5a7e7e95
Chandra9898 <chandrachowdary9898@gmail.com>
2024-12-04 07:28:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 resource "aws_instance" "one" {
 count = 5
 ami = "ami-0866a3c8686eaeeba"
-instance_type = "t2.medium"
+instance_type = "t2.micro"
 key_name = "Devops"
 vpc_security_group_ids = ["sg-05f044979e305302e"]
 tags = {
