0a35fd15bec1c76a8299c445c3ba4ae085fd3e4a
Chandra9898 <chandrachowdary9898@gmail.com>
2024-12-04 07:32:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ count = 5
 ami = "ami-0866a3c8686eaeeba"
 instance_type = "t2.micro"
 key_name = "Devops"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
 tags = {
 Name = var.instance_names[count.index]
 }
